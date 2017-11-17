31fba6d46b2ee6cd8a344ecbd2ed2fba546d8842
chenweigao <297859260@qq.com>
2017-11-17 23:26:40 +0800

    11-17 23:26

--- pycharm/web_parse2.py
+++ pycharm/web_parse2.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 
-file = 'C:/Users/Administrator/Videos/Plan-for-combating-master/week1/1_2/1_2answer_of_homework/1_2_homework_required/index.html'
+file = 'C:/Users/weigao/Videos/Plan-for-combating-master/week1/1_2/1_2answer_of_homework/1_2_homework_required/index.html'
 
-with open(file, 'r') as web_data: 
+with open(file, 'r') as web_data:
     Soup = BeautifulSoup(web_data, 'lxml')
     titles = Soup.select(
         'body > div > div > div.col-md-9 > div > div > div > div.caption > h4 > a')
@@ -10,11 +10,13 @@ with open(file, 'r') as web_data:
         'body > div > div > div > div > div > div > img')
     rates = Soup.select('body > div > div > div > div > div > div > div > h4')
 
-    print(titles,sep='\n-----------------------------------\n ')
+    stars = Soup.select('body > div > div > div.col-md-9 > div > div > div > div.ratings > p:nth-child(2)')
+    # print(titles, sep='\n-----------------------------------\n ')
 
-
-
-
-
-    # ''
-    # 'body > div:nth-child(2) > div > div.col-md-9 > div:nth-child(2) > div:nth-child(8) > div > div.caption > h4:nth-child(2) > a'
+for title,image,rate,star in zip(titles,images,rates,stars): 
+    data = {
+        'title': title.get_text(),
+        'image': image.get_text('src'),
+        'rate': rate.get_text(),
+        'star': 
+    }
