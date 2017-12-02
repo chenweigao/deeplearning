c03ed5200d383329be37cd5ee52182b0aedbf6bb
chenweigao <297859260@qq.com>
2017-12-02 23:15:45 +0800

    mnist_modification

---
+++ cookbook_code/ascii.py
@@ -0,0 +1,3 @@
+ascii = [(i,chr(i)) for i in range(122) if i > 64]
+for _ in ascii:
+    print(_)
--- cookbook_code/namedtuple.py
+++ cookbook_code/namedtuple.py
@@ -5,3 +5,4 @@ xian = City('xian','CN','5300',(34.123,389.231))
 print(xian)
 
 print(City._fields)
+
